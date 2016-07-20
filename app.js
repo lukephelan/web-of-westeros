1168cd6407069634959683ecceb391402d04f32c
lukephelan <luke.phelan@hotmail.com>
2016-07-20 10:10:41 +1000

    Initial commit, third project version

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var neo4j = require('node-neo4j');
+var cors = require('cors');
+var app = express();
+
+app.use(cors());
+app.use(express.static(__dirname + '/'));
+
+// Create a db object connected to the Neo4j database
+db = new neo4j('http://neo4j:password@localhost:7474');
+
+app.get('/', function (req, res) {
+    // db.cypherQuery("START N=NODE(*) RETURN N", function(err, result){
+    //     if(err) throw err;
+    //
+    //     res.send(result.data); // delivers an array of query results
+    // });
+    res.render('index');
+});
+
+// Create an array to store the database results
+var object = [[],[]];
+
+app.get('/json', function (req, res){
+    console.log("Hello");
+    // Retrieve all nodes from the database
+    db.cypherQuery("START N=NODE(*) RETURN {name: N.name, group: labels(N)[0]}", function(err, result){
+        if(err) throw err;
+        // console.log(result.data);
+        // console.log(JSON.stringify(result));
+        // console.log(object);
+        object[0].push(JSON.stringify(result.data));
+        // console.log(object);
+        // res.send(result.data); // delivers an array of query results
+    });
+    // Retrieve all relationships from the database
+    db.cypherQuery("START n=node(*) MATCH (n)-[r]->(m) RETURN {source: n.name, target: m.name, value: type(r)}", function(err, result){
+        if(err) throw err;
+        // console.log(result.data);
+        // console.log(JSON.stringify(result));
+        // res.send(result.data); // delivers an array of query results
+        object[1].push(JSON.stringify(result.data));
+    });
+    // object = JSON.parse(object);
+
+    // Store the nodes and links in an object and send it
+    var nodes = object[0];
+    var links = object[1];
+    res.send({nodes, links});
+});
+
+app.listen(3000, function() {
+  console.log('Winter is coming!');
+});
---
+++ got_renderer.js
@@ -0,0 +1,155 @@
+// Setup our constants for the SVG
+var width = 1440,
+    height = 800,
+    graph;
+
+// Setup the colour scale
+var color = d3.scale.category20b();
+
+// Setup the force layout
+var force = d3.layout.force()
+    .charge(-400)
+    .linkDistance(100)
+    .size([width, height]);
+
+// Append a SVG to the body of the html page and ssign this SVG as an object
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+//Read the data from the mis element
+// var mis = document.getElementById('mis').innerHTML;
+// var graph = JSON.parse(mis);
+
+// Don't do it like above, do it like this. Get the data from a json file.
+d3.json('./info/miserables2.json', function(err, json){
+    if (err) throw err;
+    graph = json;
+    // return json;
+    update();
+});
+
+function update(){
+
+    //Create the graph data structure out of the json data
+    force.nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+    //Create all the line svgs but without locations yet
+    var link = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", 1);
+    // });
+
+    var node = svg.selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .call(force.drag);
+    node.append("circle")
+        .attr("r", 8)
+        .style("fill", function (d) {
+        return color(d.group);
+    })
+    node.append("text")
+          .attr("dx", 10)
+          .attr("dy", ".35em")
+          .text(function(d) { return d.name });
+
+
+
+         //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+    force.on("tick", function () {
+        link.attr("x1", function (d) {
+            return d.source.x;
+        })
+            .attr("y1", function (d) {
+            return d.source.y;
+        })
+            .attr("x2", function (d) {
+            return d.target.x;
+        })
+            .attr("y2", function (d) {
+            return d.target.y;
+        });
+        d3.selectAll("circle").attr("cx", function (d) {
+            return d.x;
+        })
+            .attr("cy", function (d) {
+            return d.y;
+        });
+        d3.selectAll("text").attr("x", function (d) {
+            return d.x;
+        })
+            .attr("y", function (d) {
+            return d.y;
+        });
+    });
+
+// ===== COLLISION DETECTION =====
+
+    var padding = 1, // separation between circles
+        radius=8;
+    function collide(alpha) {
+      var quadtree = d3.geom.quadtree(graph.nodes);
+      return function(d) {
+        var rb = 2*radius + padding,
+            nx1 = d.x - rb,
+            nx2 = d.x + rb,
+            ny1 = d.y - rb,
+            ny2 = d.y + rb;
+        quadtree.visit(function(quad, x1, y1, x2, y2) {
+          if (quad.point && (quad.point !== d)) {
+            var x = d.x - quad.point.x,
+                y = d.y - quad.point.y,
+                l = Math.sqrt(x * x + y * y);
+              if (l < rb) {
+              l = (l - rb) / l * alpha;
+              d.x -= x *= l;
+              d.y -= y *= l;
+              quad.point.x += x;
+              quad.point.y += y;
+            }
+          }
+          return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        });
+      };
+    }
+
+
+    // ===== SEARCH FUNCTION =====
+
+    var optArray = [];
+    for (var i = 0; i < graph.nodes.length - 1; i++) {
+        optArray.push(graph.nodes[i].name);
+    }
+    optArray = optArray.sort();
+    $(function () {
+        $("#search").autocomplete({
+            source: optArray
+        });
+    });
+
+};
+
+function searchNode() {
+    //find the node
+    var selectedVal = document.getElementById('search').value;
+    var node = svg.selectAll(".node");
+    if (selectedVal == "none") {
+        node.style("stroke", "white").style("stroke-width", "1");
+    } else {
+        var selected = node.filter(function (d, i) {
+            return d.name != selectedVal;
+        });
+        selected.style("opacity", "0");
+        var link = svg.selectAll(".link")
+        link.style("opacity", "0");
+        d3.selectAll(".node, .link").transition()
+            .duration(5000)
+            .style("opacity", 1);
+    }
+}
