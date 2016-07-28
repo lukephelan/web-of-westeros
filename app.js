7f18859c52eccdfce856438ca974ac0e3b3c5477
lukephelan <luke.phelan@hotmail.com>
2016-07-28 14:25:58 +1000

    Still trying to get around issue with the module

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var apiProxy = proxy('https://gameofthrones.wikia.com/api/v1/Articles/AsSimpleJs
 var app = express(); // Set app as express
 
 
-// 
+//
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -17,7 +17,7 @@ var app = express(); // Set app as express
 app.use(express.static(__dirname + '/')); // Allow access to static page
 
 app.use(cors()); // Using cors to allow cross-origin requests
-
+app.use(apiproxy);
 // Render index.html at the root
 app.get('/', function(req, res){
     res.render('index');
