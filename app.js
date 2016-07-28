9ad7edc18b14ae763434a106680a338074a069a0
lukephelan <luke.phelan@hotmail.com>
2016-07-28 14:21:38 +1000

    Trying to use a proxy module to get around cross origin

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express'); // Require express
 var cors = require('cors'); // Required for cross-origin requests
+var proxy = require('http-proxy-middleware');
+
+var apiProxy = proxy('https://gameofthrones.wikia.com/api/v1/Articles/AsSimpleJson?id=');
+
 var app = express(); // Set app as express
 
 
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(express.static(__dirname + '/')); // Allow access to static page
 
