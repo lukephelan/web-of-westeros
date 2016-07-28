a694e88273294c5fd224c20ac8f4a553431fde0d
lukephelan <luke.phelan@hotmail.com>
2016-07-28 15:24:46 +1000

    Took out all that code. The cross origin works but only with a Chrome add-on

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 var express = require('express'); // Require express
 var cors = require('cors'); // Required for cross-origin requests
-var proxy = require('http-proxy-middleware');
 var app = express(); // Set app as express
-
-
-//
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.use(express.static(__dirname + '/')); // Allow access to static page
 
 app.use(cors()); // Using cors to allow cross-origin requests
 
-app.use(proxy({
-        target: 'https://gameofthrones.wikia.com/api/v1/Articles/AsSimpleJson?id=',
-        // changeOrigin: true,
-        // onProxyRes: function (proxyRes, req, res) {
-        //     console.log("hello");
-        //     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-        // }
-    })
-);
+app.use(express.static(__dirname + '/')); // Allow access to static page
 
 // Render index.html at the root
 app.get('/', function(req, res){
