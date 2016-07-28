2d48647a8aac2cc55a5a2954c9497793cf59e81c
lukephelan <luke.phelan@hotmail.com>
2016-07-28 14:34:06 +1000

    Trying to fiddle with some settings

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express'); // Require express
 var cors = require('cors'); // Required for cross-origin requests
 var proxy = require('http-proxy-middleware');
 
-// var apiProxy = proxy('https://gameofthrones.wikia.com/api/v1/Articles/AsSimpleJson?id=');
+var apiProxy = proxy({
+    target: 'https://gameofthrones.wikia.com/api/v1/Articles/AsSimpleJson?id=',
+    // changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
+    logLevel: 'debug'
+});
 
 var app = express(); // Set app as express
 
@@ -18,7 +22,7 @@ app.use(express.static(__dirname + '/')); // Allow access to static page
 
 app.use(cors()); // Using cors to allow cross-origin requests
 // app.use(apiProxy);
-app.use(proxy({target: 'https://gameofthrones.wikia.com/api/v1/Articles/AsSimpleJson?id='}));
+app.use(apiProxy);
 
 // Render index.html at the root
 app.get('/', function(req, res){
